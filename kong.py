ef618a3a3ab7e15a1f1fbe694f53d3f292e2adf7
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2022-11-28 19:22:32 +0800

    Add files via upload

--- kong.py
+++ kong.py
@@ -442,13 +442,13 @@ judge_res.to_csv('res_kong.csv')
 content = create_html_table(judge_res.head(10), f'判断日期{date_value}')
 #设置服务器所需信息
 #163邮箱服务器地址
-mail_host = 'smtp.gmail.com'  
+mail_host = 'smtp.163.com'  
 #163用户名
-mail_user = 'leedaowei1989'  
+mail_user = 'lee_daowei'  
 #密码(部分邮箱为授权码) 
-mail_pass = 'gcirnxmymgwzfgvb'   
+mail_pass = 'KIGWYJYDAOXLFRFB'   
 #邮件发送方邮箱地址
-sender = 'leedaowei1989@gmail.com'  
+sender = 'lee_daowei@163.com' 
 #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
 receivers = ['lee_daowei@163.com']  
 context = f'做多日期判断{date_value}'
