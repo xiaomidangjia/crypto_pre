33dc69973484609230fa955c64a02f82fb0ab47f
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2022-11-28 21:45:03 +0800

    Add files via upload

--- duo.py
+++ duo.py
@@ -441,9 +441,9 @@ content = create_html_table(judge_res.head(10), f'判断日期{date_value}')
 #163邮箱服务器地址
 mail_host = 'smtp.163.com'  
 #163用户名
-mail_user = 'lee_daowei'  
+mail_user = 'lee_daowei@163.com'  
 #密码(部分邮箱为授权码) 
-mail_pass = 'KIGWYJYDAOXLFRFB'   
+mail_pass = 'GKXGKVGTYBGRMAVE'   
 #邮件发送方邮箱地址
 sender = 'lee_daowei@163.com'  
 
--- send_email.py
+++ send_email.py
@@ -2,7 +2,6 @@ import smtplib
 from email.mime.text import MIMEText
 
 def email_sender(mail_host,mail_user,mail_pass,sender,receivers,context,content):
-    #设置email信息
     #邮件内容设置
     message = MIMEText(content,'plain','utf-8')
     #邮件主题       
@@ -14,9 +13,9 @@ def email_sender(mail_host,mail_user,mail_pass,sender,receivers,context,content)
     
     #登录并发送邮件
     try:
-        smtpObj = smtplib.SMTP() 
+        smtpObj = smtplib.SMTP_SSL(mail_host, 465)
         #连接到服务器
-        smtpObj.connect(mail_host,25)
+        #smtpObj.ehlo(mail_host)
         #登录到服务器
         smtpObj.login(mail_user,mail_pass) 
         #发送
@@ -27,17 +26,3 @@ def email_sender(mail_host,mail_user,mail_pass,sender,receivers,context,content)
         print('success')
     except smtplib.SMTPException as e:
         print('error',e) #打印错误
-if __name__ == '__main__':
-    #设置服务器所需信息
-    #163邮箱服务器地址
-    mail_host = 'smtp.163.com'  
-    #163用户名
-    mail_user = 'lee_daowei'  
-    #密码(部分邮箱为授权码) 
-    mail_pass = 'KIGWYJYDAOXLFRFB'   
-    #邮件发送方邮箱地址
-    sender = 'lee_daowei@163.com'  
-    #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-    receivers = ['lee_daowei@163.com']  
-    context = '新年快乐'
-    email_sender(mail_host,mail_user,mail_pass,sender,receivers,context)
